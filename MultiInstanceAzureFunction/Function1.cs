7a48252211481e95b9bd79a628343d3062b9c7ed
vikashkritant <vikashkritant@gmail.com>
2024-01-31 21:25:32 +0530

    Trying to fix binding.

--- MultiInstanceAzureFunction/Function1.cs
+++ MultiInstanceAzureFunction/Function1.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 using System.Threading;
-using Azure.Messaging.ServiceBus;
+//using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -12,24 +12,24 @@ namespace MultiInstanceAzureFunction
         private string messageId;
 
         [FunctionName("ProcessQueueMessage")]
-        public void Run([ServiceBusTrigger("zipqueue",Connection = "ServiceBusConnectionString")]ServiceBusReceivedMessage msg, ILogger log)
+        public void Run([ServiceBusTrigger("zipqueue",Connection = "ServiceBusConnectionString")]string msg, string messageId, ILogger log)
         {
-            log.LogInformation($"Function- started");                                    
-            log.LogInformation($"Function- Received message with id: {msg.MessageId}");
-            if(messageId==msg.MessageId)
-            {
-                log.LogInformation($"skipping message as received message id is: {msg.MessageId} and delivery count is: {msg.DeliveryCount}");
-            }
+            log.LogInformation($"Function- started {messageId}");                                    
+            //log.LogInformation($"Function- Received message with id: {msg.MessageId}");
+            //if(messageId==msg.MessageId)
+            //{
+            //    log.LogInformation($"skipping message as received message id is: {msg.MessageId} and delivery count is: {msg.DeliveryCount}");
+            //}
             if (string.IsNullOrEmpty(messageId))
             {
-                messageId = msg.MessageId;
+                //messageId = msg.MessageId;
                 try
                 {
                     //echo "Mounting drive"
                     //az webapp config storage-account add--resource - group vikashrg--name MultiInstanceAzureFunction --custom - id myfileshareid--storage - type AzureFiles--share - name myfileshare--account - name vikashrg8582--mount - path / vikash - files--access - key jzyAF9ufbN4TyM6nVbXXI4XoXZiFJklKXl82VFeHI2riAgBuVQCVkg2I / zIh7bQxpL44LUY2uvFb + AStsom9pA ==
                     //echo "Mounting drive done"
-                    UnZipFileProcessor unzipper = new UnZipFileProcessor(log);
-                    unzipper.ProcessFileShare();
+                    //UnZipFileProcessor unzipper = new UnZipFileProcessor(log);
+                    //unzipper.ProcessFileShare();
                 }
                 catch (Exception ex)
                 {
