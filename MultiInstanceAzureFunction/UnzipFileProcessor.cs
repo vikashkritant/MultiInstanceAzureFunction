ddbcaaefc27831d39554f7c236b1a18ee969b970
vikashkritant <vikashkritant@gmail.com>
2024-04-10 21:59:35 +0530

    Pushed for deploy

--- MultiInstanceAzureFunction/UnzipFileProcessor.cs
+++ MultiInstanceAzureFunction/UnzipFileProcessor.cs
@@ -237,7 +237,6 @@ namespace MultiInstanceAzureFunction
                     return false;// await Task.FromResult(false);
                 }
             }
-
             return true;// await Task.FromResult(true);
         }
 
